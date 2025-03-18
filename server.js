374de5dd25a78dc3964a8f41a421152d9e374361
codebluenick <codebluenick@gmail.com>
2025-03-17 22:30:30 -0700

    Connect to MongoDB
    Connects to database and starts listening

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
+const port = process.env.PORT || 3001; // Port defined in .env
 
-const port = 3001;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+// Connect to MongoDB, uri stored in .env
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to the Database !!!")
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`); // Run server after connection with the database 
+    });
+})
+.catch(() => {
+    console.log("Connection to the Database failed !!!")
 });
