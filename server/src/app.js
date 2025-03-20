2bdc4d7187fb1a2ebd4733baf5bdfafd5e75a3e7
codebluenick <codebluenick@gmail.com>
2025-03-19 19:45:25 -0700

    Change port on server

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
