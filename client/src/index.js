c13d171a75c49934b59870c9c08aafb6547b126e
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2025-04-05 10:53:12 -0700

    Nested browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <GoogleOAuthProvider clientId="561885685936-u64n721cmhllg50v4jfhujtj4u88fqrb.apps.googleusercontent.com">
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </GoogleOAuthProvider>,
   document.getElementById('root')
 );
