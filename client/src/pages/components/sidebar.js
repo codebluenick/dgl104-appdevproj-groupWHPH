3087f2a2485854d453cc19f7d2ad5ce9c82af34c
Hardik D Vaghasiya <58773340+Hardikvaghasiya@users.noreply.github.com>
2025-04-05 12:00:50 -0700

    Update Sidebar code and design

--- client/src/pages/components/sidebar.js
+++ client/src/pages/components/sidebar.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2 className="logo">STMS</h2>
+      <div className="logo">STMS</div>
       <ul className="nav">
-        <li>Home</li>
-        <li>My Tasks</li>
-        <li>My Plan</li>
-        <li>Inbox</li>
-        <li>People</li>
-        <li>Reporting</li>
+        <li><i className="fas fa-home"></i> Home</li>
+        <li><i className="fas fa-tasks"></i> My Tasks</li>
+        <li><i className="fas fa-calendar"></i> My Plan</li>
+        <li><i className="fas fa-inbox"></i> Inbox</li>
+        <li><i className="fas fa-users"></i> People</li>
+        <li><i className="fas fa-chart-line"></i> Reporting</li>
       </ul>
     </div>
   );
